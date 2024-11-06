c840c87a1fea95824ef30c5b188784ce5e943820
JacobA12 <jacob.adams121202@gmail.com>
2024-11-05 17:05:24 -0800

    Account Class

--- src/com/banking/Account.java
+++ src/com/banking/Account.java
@@ -7,9 +7,32 @@ public class Account {
     private int accountNumber;
     private BigDecimal balance;
 
-    public Account(String ownerName, int accountNumber){
+    public Account(String ownerName, int accountNumber) {
         this.ownerName = ownerName;
         this.accountNumber = accountNumber;
         this.balance = new BigDecimal("0");
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void deposit(BigDecimal amount) {
+        balance = balance.add(amount);
+    }
+
+    public boolean withdraw(BigDecimal amount) {
+        if (balance.compareTo(amount) == 1) {
+            balance = balance.subtract(amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "ownerName='" + ownerName + '\'' + ", accountNumber=" + accountNumber + ", balance=" + balance + '}';
     }
 }
