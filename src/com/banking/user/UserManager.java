24ce7e4bf289eae0bd0ddf070aeea364a25a6de1
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-11-05 20:13:44 -0800

    Added usermanager class

--- src/com/banking/user/UserManager.java
+++ src/com/banking/user/UserManager.java
@@ -1,4 +1,34 @@
 package com.banking.user;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Base64;
 public class UserManager {
+    private Map<String, User> users;
+
+    public UserManager(){
+        this.users = new HashMap<>();
+    }
+
+    public boolean registerUser(String username, String password){
+        if(users.containsKey(username)){
+            return false;
+        }
+        User newUser = new User(username, password);
+        users.put(username, newUser);
+        return true;
+    }
+
+    public boolean authenticateUser(String username, String password){
+        User user = users.get(username);
+        if(user == null){
+            return false;
+        }
+        String hashedPassword = hashPassword(password, user.getSalt());
+        return hashedPassword.equals(user.getHashedPassword());
+    }
+
+    private String hashPassword(String password, String salt){
+        return Base64.getEncoder().encodeToString((password+salt).getBytes());
+    }
+
 }
