dfd6b3a99fab4b25980bde8f1d0966705c1a6787
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-11-05 20:22:21 -0800

    integrated users into Bank class

--- src/com/banking/Bank.java
+++ src/com/banking/Bank.java
@@ -1,32 +1,47 @@
 package com.banking;
 
-import java.math.BigDecimal;
+import com.banking.user.UserManager;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Bank {
     private static int nextAccountNumber = 1000;
     private Map<Integer, Account> accounts;
+    private UserManager userManager;
 
-    public Bank(){
+    public Bank() {
         this.accounts = new HashMap<>();
+        this.userManager = new UserManager();
     }
 
-    public int createAccount(String accountHolder){
+    public boolean registerUser(String username, String password) {
+        return userManager.registerUser(username, password);
+    }
+
+    public boolean authenticateUser(String username, String password) {
+
+        return userManager.authenticateUser(username, password);
+    }
+
+    public int createAccount(String username, String password, String accountHolder) {
+        if (!userManager.authenticateUser(username, password)) {
+            throw new IllegalStateException("User not authenticated");
+        }
         int accountNumber = nextAccountNumber++;
         Account newAccount = new Account(accountNumber, accountHolder);
         accounts.put(accountNumber, newAccount);
         return accountNumber;
     }
 
-    public Account getAccount(int accountNumber){
+    public Account getAccount(int accountNumber) {
         return accounts.get(accountNumber);
     }
 
-    public void deleteAccount(int accountNumber){
+
+    public void deleteAccount(int accountNumber) {
         accounts.remove(accountNumber);
     }
-
 
 
 }
